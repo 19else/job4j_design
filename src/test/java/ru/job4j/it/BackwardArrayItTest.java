784cf72f13616b8accb4371cb013b8119695c8af
19else <159753ElSe->
2022-02-01 10:52:48 +0300

    1. Что такое итератор. [#4951]

--- src/test/java/ru/job4j/it/BackwardArrayItTest.java
+++ src/test/java/ru/job4j/it/BackwardArrayItTest.java
@@ -10,7 +10,7 @@ import java.util.NoSuchElementException;
 public class BackwardArrayItTest {
 
     @Test
-    public void whenMultiCallhasNextThenTrue() {
+    public void whenMultiCellhasNextThenTrue() {
         BackwardArrayIt it = new BackwardArrayIt(
                 new int[]{1, 2, 3}
         );
