166143a39564dbbd54c414d1b983fcbba6958bb6
19else <159753ElSe->
2022-05-03 21:50:04 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539 #296537]

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -4,47 +4,34 @@ import java.util.NoSuchElementException;
 import java.util.Iterator;
 
 public class MatrixIt implements Iterator<Integer> {
-private final int[][] data;
-private int column = 0; //столбец
-private int line = 0;  // строка
+    private final int[][] data;
+    /**
+     * стобец
+     */
+    private int column = 0;
+    /**
+     * строка
+     */
+    private int line = 0;
 
-
-public MatrixIt(int[][] data) {
-    this.data = data;
-
-}
-
-@Override
-    public boolean hasNext() {
-    while (line > data[line].length) {
-        column++;
+    public MatrixIt(int[][] data) {
+        this.data = data;
     }
-    return column < data.length
-            && line < data[column].length;
-    //  return column < data.length
-//         && line < data[column].length;
-}
+
+    @Override
+    public boolean hasNext() {
+        while (line > data[line].length) {
+            column++;
+        }
+        return column < data.length
+                && line < data[column].length;
+    }
 
     @Override
     public Integer next() {
-    int result;
-    result = data[column][line];
-    line++;
-    return result;
-
-    //        if (!hasNext()) {
-//            throw new NoSuchElementException();
-//        }
-//        int rsl = 0;
-//        while (column < data.length) {
-//            if (line < data[column].length) {
-//                rsl = data[column][line];
-//                line++;
-//            } else {
-//                line = 0;
-//                column++;
-//            }
-//        }
-//        return rsl;
+        int result;
+        result = data[column][line];
+        line++;
+        return result;
     }
 }
