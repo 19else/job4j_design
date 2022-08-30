70fb4a86385258faf0d369faa8377ba9a8b76f61
19else <159753ElSe->
2022-08-30 21:21:14 +0300

    Подключение библиотеки AssertJ [#504881 #336409]

--- src/test/java/ru/job4j/it/ArrayItTest.java
+++ src/test/java/ru/job4j/it/ArrayItTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.it;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
@@ -8,18 +9,18 @@ import static org.assertj.core.api.Assertions.*;
 public class ArrayItTest {
 
     @Test
-     void whenMultiCallHasNextThenTrue() {
-    ArrayIt it = new ArrayIt(
-            new int[] {1, 2, 3}
-    );
-    assertThat(it.hasNext()).isTrue();
-    assertThat(it.hasNext()).isTrue();
+    void whenMultiCallHasNextThenTrue() {
+        ArrayIt it = new ArrayIt(
+                new int[]{1, 2, 3}
+        );
+        assertThat(it.hasNext()).isTrue();
+        assertThat(it.hasNext()).isTrue();
     }
 
     @Test
-     void whenReadSequence() {
+    void whenReadSequence() {
         ArrayIt it = new ArrayIt(
-                new int[] {1, 2, 3}
+                new int[]{1, 2, 3}
         );
 
         assertThat(it.next()).isEqualTo(1);
@@ -30,9 +31,9 @@ public class ArrayItTest {
     @Test
     public void whenNextFromEmpty() throws NoSuchElementException {
         ArrayIt it = new ArrayIt(
-                new int[] {}
+                new int[]{}
         );
         Throwable thrown = catchThrowable(it::next);
         assertThat(thrown).isInstanceOf(NoSuchElementException.class);
-       }
+    }
 }
